abeeb52e19b95bf9edb1e70737eda33fbb724ccc
Kaletts-2 <ailenmorra.317@gmail.com>
2023-11-05 14:49:50 +0100

    Se agrega codigo completo

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -14,35 +14,36 @@ namespace WinFormsApp1
             char tipoTelegrama = 'o';
             int numPalabras = 0;
             double coste;
-            //Leo el telegrama
             textoTelegrama = txtTelegrama.Text;
-            // telegrama urgente?
+
+            // telegrama es urgente?
             if (rbUrgente.Checked)
                 tipoTelegrama = 'u';
-            // telegrama ordinario?
             else if (rbOrdinario.Checked)
                 tipoTelegrama = 'o';
+
             //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
+            string[] palabras = textoTelegrama.Split(' ');
+            numPalabras = palabras.Length;
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
+            {
                 if (numPalabras <= 10)
                     coste = 2.5;
                 else
-                    //Faltaba el calculo completo
                     coste = 2.5 + (0.5 * (numPalabras - 10));
-            else
+            }
             //Si el telegrama es urgente
-            if (tipoTelegrama == 'u')
+            else if (tipoTelegrama == 'u')
+            {
                 if (numPalabras <= 10)
                     coste = 5;
                 else
-                    //Faltaba el parentesis para la operación
                     coste = 5 + (0.75 * (numPalabras - 10));
+            }
             else
                 coste = 0;
             txtPrecio.Text = coste.ToString() + " euros";
-
 
         }
     }
