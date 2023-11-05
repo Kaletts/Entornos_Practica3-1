10ad4bd39f4b1e486f41d4890d8e9787282888c1
Kaletts-2 <ailenmorra.317@gmail.com>
2023-11-05 12:27:03 +0100

    Corregidos errores de practica

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -9,8 +9,9 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             string textoTelegrama;
-            char tipoTelegrama = ' ';
+            char tipoTelegrama = 'o';
             int numPalabras = 0;
             double coste;
             //Leo el telegrama
@@ -18,21 +19,28 @@ namespace WinFormsApp1
             // telegrama urgente?
             if (cbUrgente.Checked)
                 tipoTelegrama = 'u';
-            //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
+            //Obtengo el número de palabras que forma el telegrama - Esto originalmente solo obtendría el numero de letras.
+            //Creo un array para guardar las palabras
+            string[] palabras;
+            //Uso el metodo split para separar cuando haya un espacio cada palabra
+            palabras = textoTelegrama.Split(' ');
+            //El total de strings en el array es la cantidad de palabras que tendremos
+            numPalabras = palabras.Length;
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
+                    //Faltaba el calculo completo
+                    coste = 2.5 + (0.5 * (numPalabras - 10));
             else
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
                 if (numPalabras <= 10)
                     coste = 5;
                 else
-                    coste = 5 + 0.75 * (numPalabras - 10);
+                    //Faltaba el parentesis para la operación
+                    coste = 5 + (0.75 * (numPalabras - 10));
             else
                 coste = 0;
             txtPrecio.Text = coste.ToString() + " euros";
